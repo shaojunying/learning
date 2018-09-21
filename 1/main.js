b81ba3d3e6d904a01bf04d58fca47f13e7813a67
邵俊颖 <shaojunying@bupt.edu.cn>
2018-09-21 14:10:41 +0800

    v-bind还没有完成

---
+++ 1/main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+        el: '#app',
+        data: {
+            // name: 'A',
+            // age:18,
+            // sex:"男"
+            name: null,
+            age:null,
+            sex:null
+        }
+    });
---
+++ 3/main.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+        el: '#app',
+        data: {
+            url:"http://www.baidu.com",
+            img:"https://avatars3.githubusercontent.com/u/30252316?v=4"
+        }
+    });
